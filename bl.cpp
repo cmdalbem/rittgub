b242f2936a6ec7ed69528a5626841b112f9cc859
ffsbeltrao <ffsbeltrao@gmail.com>
2010-11-29 09:50:17 -0200

    Lol, tinha commitado antes e nem compilava...agora compila. Tem que implementar canDelegate(int task, int machine); ainda. On another note, implementei topological sort, e chamei logo ap?s ler a entrada. O resultado fica armazenado no vetor topSorted, para futura referencia.

--- bl.cpp
+++ bl.cpp
@@ -4,20 +4,22 @@
 #include <ctime>
 
 #define MAX_ITERATIONS 10
-#define MAX_TASKS 300
+#define MAX_TASKS 300 // THIS... IS... SPARTAAAAAAA !!!
 #define MAX_MACHINES 70
 #define N_TASKS MAX_TASKS - 1
 #define MACHINE_CICLE MAX_TASKS - 2
 
-int graph[MAX_TASKS][MAX_TASKS]; // THIS... IS... SPARTAAAAAAA !!!
+int graph[MAX_TASKS][MAX_TASKS];
 int costs[MAX_TASKS];
 int n, m, currentSolution, cicle;
-
 int atribs[MAX_MACHINES][MAX_TASKS];
+int degree[MAX_TASKS], topSorted[MAX_TASKS];
 
 void init();
 void readInput();
+void topSort();
 void printInput();
+bool canDelegate(int task, int machine){}
 void randomGreedy();
 void localSearch();
 void updateSolution();
@@ -27,12 +29,13 @@ int main() {
 	init();
 	
 	readInput();
+	topSort();
 	//printInput();
 	
 	for(int i = 0; i < MAX_ITERATIONS; i++) {
 		randomGreedy();
-		localSearch();
-		updateSolution();
+		//localSearch();
+		//updateSolution();
 	}
 	
 	printf("Menor tempo de cicle: %i\n", cicle);
@@ -40,7 +43,17 @@ int main() {
 
 
 void randomGreedy() {
+	for(int i = 0; i < MAX_TASKS; i++) {	
+		for(int j = 0; j < MAX_MACHINES; j++)
+			atribs[j][i] = 0;
+	}
 	
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < m; j++) {
+			
+			
+		}
+	}
 }
 
 void localSearch() {
@@ -50,7 +63,7 @@ void localSearch() {
 	do {
 		improved = false;
 		severalWorst = false;
-		localCicle = 0
+		localCicle = 0;
 		
 		// calculates the longest cicle between all machines
 		for(int i = 0; i < m; i++) {
@@ -63,17 +76,20 @@ void localSearch() {
 				severalWorst = true;
 		}
 		
+		currentSolution = localCicle;
+		
 		// if more then 1 machine shares the longest cicle, doesn't have better neighbor
 		if(severalWorst)
 			return;
 		
 		for(int i = 0; i < m; i++) {
 			if(i != worstIndex) {
-				if(canDelegate(atribs[worstIndex][N_TASKS-1],i) {
+				if(canDelegate(atribs[worstIndex][N_TASKS-1],i)) {
 					if(atribs[i][MACHINE_CICLE] + costs[atribs[worstIndex][N_TASKS-1]] < atribs[worstIndex][MACHINE_CICLE]) {
 						atribs[i][N_TASKS] = atribs[worstIndex][N_TASKS-1];
 						atribs[i][N_TASKS]++;
 						atribs[worstIndex][N_TASKS]--;
+						currentSolution = atribs[i][N_TASKS];
 						improved = true;
 					}
 				}
@@ -85,6 +101,38 @@ void localSearch() {
 void updateSolution() {
 	if(currentSolution < cicle)
 		cicle = currentSolution;
+}
+
+void topSort() {
+	int stack[MAX_TASKS], top, count, r, i, k;
+	
+	top = -1;
+	count = 0;
+	r = 0;
+	
+	for(i=0;i<n;i++)
+		if(!degree[i])
+			stack[++top]=i;
+			
+
+	while(top>-1)
+	{
+		i=stack[top--];
+		//printf("%i ",i);
+		topSorted[r++] = i;
+		count++;
+		for(k = 0;k < n;k++)
+		{
+			if(graph[i][k])
+				if(!(--degree[k]))
+					stack[++top]=k;
+		}
+	}
+	
+	if(count < n) {
+		printf("Impossible to sort topologicaly\n");
+		exit(1);
+	}
 }
 
 
@@ -99,6 +147,7 @@ void readInput() {
 	
 	while(i != -1 && j != -1) {
 		graph[i-1][j-1] = 1;
+		degree[j-1]++;
 		scanf("%i,%i", &i, &j);
 	}
 }
@@ -115,6 +164,7 @@ void init() {
 			graph[i][j] = 0;
 			
 		costs[i] = 0;
+		degree[i] = 0;
 		
 		for(int j = 0; j < MAX_MACHINES; j++)
 			atribs[j][i] = 0;
@@ -125,12 +175,19 @@ void init() {
 
 void printInput() {
 	
+	printf("Graph:\n");
 	for(int i = 0; i < n; i++) {
 		for(int j = 0; j < n; j++)
 			printf("%i ", graph[i][j]);
 		printf("\n");
 	}
 	
+	printf("Costs:\n");
 	for(int i = 0; i < n; i++)
 		printf("%i: %i\n", i, costs[i]);
+		
+	printf("Topological order:\n");
+	for(int i = 0; i < n; ++i)
+			printf("%i ", topSorted[i]);
+	printf("\n");
 }
