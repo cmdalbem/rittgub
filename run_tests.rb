cbc4245665ee39c7c6ccabf4614bcde4d1109b28
ffsbeltrao <ffsbeltrao@gmail.com>
2010-12-06 13:06:08 -0200

    varias coisas

--- run_tests.rb
+++ run_tests.rb
@@ -1,7 +1,9 @@
 #!/usr/bin/ruby
 
 tests =
-"Barthol2 	51 	84
+"Arc111	27	5689
+Arc111	9	16839
+Barthol2 	51 	84
 Barthol2 	27 	157
 Warnecke 	10 	155
 Warnecke 	20 	79
@@ -9,10 +11,10 @@ Scholl 	50 	1394
 Scholl 	38 	1834
 Scholl 	25 	2787
 Wee-Mag 	20 	77
-Wee-Mag 	30 	56"
-
-candidates = [1,2,3,4,5]
-randomic = [0,1,2]
+Wee-Mag 	30 	56
+"
+candidates = [0,25,50,75,100]
+randomic = [0,1,2,5,nil]
 
 tests.each_line do |t|
   t = t.split(/[\s\t]+/)
