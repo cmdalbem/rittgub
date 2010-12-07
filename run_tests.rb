31809d9ce61f9dd0bc89a9a181dc1af61f69321c
lfzawacki <lfzawacki@gmail.com>
2010-12-06 23:55:22 -0200

    codigo melhorado

--- run_tests.rb
+++ run_tests.rb
@@ -16,16 +16,16 @@ else
 end
 
 tests =
-#"Arc111	27	5689
-#Arc111	9	16839
-#Barthol2 	51 	84
-#Barthol2 	27 	157
-#Warnecke 	10 	155
-#Warnecke 	20 	79
-#Scholl 	50 	1394
-#Scholl 	38 	1834
-#Scholl 	25 	2787
-"Wee-Mag 	20 	77
+"Arc111	27	5689
+Arc111	9	16839
+Barthol2 	51 	84
+Barthol2 	27 	157
+Warnecke 	10 	155
+Warnecke 	20 	79
+Scholl 	50 	1394
+Scholl 	38 	1834
+Scholl 	25 	2787
+Wee-Mag 	20 	77
 Wee-Mag 	30 	56"
 candidates = [0,25,50,75,100]
 
